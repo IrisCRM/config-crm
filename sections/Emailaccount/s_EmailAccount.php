46ee90242fc1de0fec0d2859a32d54ce9662e17a
miv <mivxxx@gmail.com>
2017-12-05 10:00:01 +0300

    Auto insert default mailbox on email account save

---
+++ sections/Emailaccount/s_EmailAccount.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Iris\Config\CRM\sections\Emailaccount;
+
+use Config;
+use Iris\Iris;
+use IrisDomain;
+
+class s_EmailAccount extends Config
+{
+    function __construct($Loader)
+    {
+        parent::__construct($Loader, array('common/Lib/lib.php'));
+    }
+
+    public function onAfterPost($table, $id, $OldData, $NewData) {
+        $protocolCode = GetArrayValueByName(
+            $NewData['FieldValues'], 'fetch_protocol');
+        $imapTypeCode = IrisDomain::getDomain('d_fetch_protocol')->
+            get('imap', 'code', 'db_value');
+
+        if ($protocolCode == $imapTypeCode && !$OldData) {
+            $this->insertDefaultMailbox($id);
+        }
+    }
+
+    protected function insertDefaultMailbox($id) {
+        $sql = "insert into iris_emailaccount_mailbox
+            (id, name, displayname, lastuid, emailaccountid) values
+            (iris_genguid(), :name, :displayname, :lastuid, :emailaccountid)
+        ";
+        $cmd =$this->connection->prepare($sql);
+        $cmd->execute(array(
+            ":name" => "INBOX",
+            ":displayname" => "Входящие",
+            ":lastuid" => 0,
+            ":emailaccountid" => $id,
+        ));
+    }
+}
